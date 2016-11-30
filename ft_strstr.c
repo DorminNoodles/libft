c726367b160d66723bb0b9382bd69bafa52ae331
Loic CHETY <lchety@e3r12p16.42.fr>
2016-11-30 22:40:55 +0100

    all

---
+++ ft_strstr.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/06 16:43:35 by lchety            #+#    #+#             */
+/*   Updated: 2016/11/30 13:00:01 by lchety           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	return (ft_strnstr(haystack, needle, ft_strlen(haystack) + 1));
+}
