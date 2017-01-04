a74d6728bfeaaaf48cc44c46d3534a00a75300f8
Loic CHETY <lchety@e3r12p13.42.fr>
2017-01-04 16:04:56 +0100

    ft_strjoin very very faster

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/08 10:43:42 by lchety            #+#    #+#             */
-/*   Updated: 2016/11/13 16:45:43 by lchety           ###   ########.fr       */
+/*   Updated: 2017/01/04 16:04:06 by lchety           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (str == NULL)
 		return (NULL);
 	ft_memcpy(str, s1, ft_strlen(s1));
-	ft_memcpy(&str[ft_strlen(s1)], s2, ft_strlen(s2));
+	str += ft_strlen(s1);
+	ft_memcpy(str, s2, ft_strlen(s2));
 	return (str);
 }
