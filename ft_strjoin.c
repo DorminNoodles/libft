601923e85d440028e5e789b586078b431b304492
Loic CHETY <lchety@e3r12p13.42.fr>
2017-01-04 16:14:35 +0100

    ft_strjoin very very faster

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/08 10:43:42 by lchety            #+#    #+#             */
-/*   Updated: 2017/01/04 16:04:06 by lchety           ###   ########.fr       */
+/*   Updated: 2017/01/04 16:14:26 by lchety           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char *str;
+	size_t size1;
+	size_t size2;
 
-	str = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	size1 = ft_strlen(s1);
+	size2 = ft_strlen(s2);
+
+	str = ft_strnew(size1 + size2);
 	if (str == NULL)
 		return (NULL);
-	ft_memcpy(str, s1, ft_strlen(s1));
-	str += ft_strlen(s1);
-	ft_memcpy(str, s2, ft_strlen(s2));
+	ft_memcpy(str, s1, size1);
+	str += size1;
+	ft_memcpy(str, s2, size2);
 	return (str);
 }
